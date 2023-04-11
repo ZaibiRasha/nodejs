2fad6470d5efe48d81ec94202df601e85f3082e2
ZaibiRasha <racha.zaibi@wind-consulting-tunisia.com>
2023-04-11 10:30:15 +0100

    nodemon package

--- 5-web/1-hello-world.js
+++ 5-web/1-hello-world.js
@@ -1,7 +1,8 @@
 const http = require('http');
 
 const server = http.createServer((req, res) => {
-  res.end('Hello World\n');
+  console.dir(req, { depth : 0 });
+  res.end('Hello World !\n');
 });
 
 server.listen(4242, () => {
