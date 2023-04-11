b214eafdc8bbcbb8576c924bbe58490d5f70c5da
ZaibiRasha <racha.zaibi@wind-consulting-tunisia.com>
2023-04-11 09:14:30 +0100

    events  subscibe and listen to events

--- 4-modules/7-event-emitters/example.js
+++ 4-modules/7-event-emitters/example.js
@@ -1,4 +1,25 @@
 const EventEmitter = require('events');
+const myEmitter = new EventEmitter();
+
+/* setImmediate call wil execute the listner
+   after the rest of the code */ 
+setImmediate(()=>{
+myEmitter.emit('TEST_EVENT');
+})
+
+/* on : to subsucribe to the event */
+myEmitter.on( 'TEST_EVENT' , ()=>{
+  console.log( 'TEST_EVENT was fired')
+});
+myEmitter.on( 'TEST_EVENT' , ()=>{
+  console.log( 'TEST_EVENT was fired')
+});
+myEmitter.on( 'TEST_EVENT' , ()=>{
+  console.log( 'TEST_EVENT was fired')
+});
+
+/* emit : to listen to the event */
+// myEmitter.emit('TEST_EVENT');
 
 // Streams are Event Emitters
 // process.stdin, process.stdout
